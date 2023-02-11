f5cf1041d1d38443529adb4984631ce2bcdd7506
Jefferson Simão Gonçalves <411493+jeffersonsimaogoncalves@users.noreply.github.com>
2023-02-11 20:22:47 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,10 @@ mkdir -p /github/home/.ssh
 
 eval $(ssh-agent -s)
 
-echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo -e "StrictHostKeyChecking yes" >> /etc/ssh/ssh_config
 
 install -m 600 -D /dev/null ~/.ssh/id_rsa
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-ssh-keyscan -H $SSH_HOST > ~/.ssh/known_hosts
 ssh-add ~/.ssh/id_rsa
 
 deployer --version
