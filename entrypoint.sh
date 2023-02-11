9684b5a5537a444ff46914502c9cfbaca0659290
Jefferson Simão Gonçalves <411493+jeffersonsimaogoncalves@users.noreply.github.com>
2023-02-11 20:07:13 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,7 @@ else
     CMD_ARGS="$@"
 fi
 
-mkdir -p /github/home/.ssh
-
 eval $(ssh-agent -s)
-
-echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
-chmod 600 /tmp/id_rsa
-ssh-add /tmp/id_rsa
 
 deployer --version
 deployer $CMD_ARGS
