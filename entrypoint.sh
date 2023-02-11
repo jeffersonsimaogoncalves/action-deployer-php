b715880f73ab8fec3459a9deb164804b5081cc21
Jefferson Simão Gonçalves <411493+jeffersonsimaogoncalves@users.noreply.github.com>
2023-02-11 19:37:40 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ mkdir -p /github/home/.ssh
 
 eval $(ssh-agent -s)
 
-echo -e "StrictHostKeyChecking yes" >> /etc/ssh/ssh_config
+echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
 ssh-add /tmp/id_rsa
